92d9919fdfcad3831bf1160de5b52ce7c633dc4d
LuanEduardoS <luaneduardolima186@gmail.com>
2022-02-17 21:02:24 -0300

    l
    ...

---
+++ aula012/ex011.js
@@ -0,0 +1,9 @@
+var idade = 22
+if (idade < 18) {
+    console.log('Menor de Idade')
+} else if (idade < 18 || idade > 65) {
+    console.log('Voto Opcional')
+}
+else {
+    console.log('Voto Obrigatório')
+}
