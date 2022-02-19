f8151dca1fb21ad31d8e287174d5959e114abd46
LuanEduardoS <luaneduardolima186@gmail.com>
2022-02-19 17:29:10 -0300

    Create exemplo.js
    ...

---
+++ aula012/exemplo.js
@@ -0,0 +1,11 @@
+var danny = 0
+var zarby = 0
+if (zarby > danny) {
+    console.log('Zarby é melhor que o Danny')
+} else if (danny > zarby) {
+    console.log('Danny é melhor do que o Zarby')
+}
+else if (danny == zarby) {
+    console.log(`Os dois empataram`)
+}
+console.log(`De qualquer jeito, Luan tá upando sem precisar fazer nada :)`)
